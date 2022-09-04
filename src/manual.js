8dc10da6dc1079922f63988928f0fa2bf43f9065
xiangyu <3170102889@zju.edu.cn>
2022-09-04 18:00:31 +0800

    resolve: #29 copy rarely used tags
    fix: build bug

--- src/manual.js
+++ src/manual.js
@@ -1,3 +1,54 @@
+class CopyHelper {
+  constructor() {
+    this.transferable = Components.classes[
+      "@mozilla.org/widget/transferable;1"
+    ].createInstance(Components.interfaces.nsITransferable);
+    this.clipboardService = Components.classes[
+      "@mozilla.org/widget/clipboard;1"
+    ].getService(Components.interfaces.nsIClipboard);
+  }
+
+  addText(source, type) {
+    const str = Components.classes[
+      "@mozilla.org/supports-string;1"
+    ].createInstance(Components.interfaces.nsISupportsString);
+    str.data = source;
+    this.transferable.addDataFlavor(type);
+    this.transferable.setTransferData(type, str, source.length * 2);
+    return this;
+  }
+
+  addImage(source) {
+    const io = Components.classes[
+      "@mozilla.org/network/io-service;1"
+    ].getService(Components.interfaces.nsIIOService);
+    const channel = io.newChannel(source, null, null);
+    const input = channel.open();
+    const imgTools = Components.classes[
+      "@mozilla.org/image/tools;1"
+    ].getService(Components.interfaces.imgITools);
+
+    const buffer = NetUtil.readInputStreamToString(input, input.available());
+    const container = imgTools.decodeImageFromBuffer(
+      buffer,
+      buffer.length,
+      channel.contentType
+    );
+
+    this.transferable.addDataFlavor(channel.contentType);
+    this.transferable.setTransferData(channel.contentType, container, -1);
+    return this;
+  }
+
+  copy() {
+    this.clipboardService.setData(
+      this.transferable,
+      null,
+      Components.interfaces.nsIClipboard.kGlobalClipboard
+    );
+  }
+}
+
 export default {
   manual: {
     io: {},
@@ -54,8 +105,13 @@ export default {
             out.push(k);
           }
         }
+        new CopyHelper().addText(out.join(","), "text/unicode").copy();
         tagsBox.value =
           (tagsBox.value.length ? tagsBox.value + "," : "") + out.join(",");
+        Zotero.ZoteroTag.showProgressWindow(
+          "Rarely Used Tags Copied",
+          out.join(",")
+        );
       }
     },
   },
